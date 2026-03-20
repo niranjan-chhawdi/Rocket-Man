3ff7c0202541a8e5e2821de50df4c7ebb235a12f
Niranjan Chhawdi <chhawdiniranjan@gmail.com>
2026-03-20 06:00:30 -0700

    Menu updated

--- Assets/Script/InventoryController.cs
+++ Assets/Script/InventoryController.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class InventoryController : MonoBehaviour
 {
+    public GameObject inventoryPanel;
+    public GameObject slotPrefab;
+    public int numberOfSlots = 20;
+
+    public GameObject[] itemPrefabs; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
+        for (int i = 0; i < numberOfSlots; i++)
+        {
+            Slot slot = Instantiate(slotPrefab, inventoryPanel.transform).GetComponent<Slot>();
+            if (i < itemPrefabs.Length)
+            {
+                GameObject item = Instantiate(itemPrefabs[i], slot.transform);
+                item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                slot.currentItem = item;
+            }
+        }
+    }        
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
+
